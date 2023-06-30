0891bfe6fbcc69412f5bbd5459886cf752e02baf
xinmiao1995 <kyokushinmyou@gmail.com>
2023-06-30 14:03:04 +0900

    fix loc

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,4 @@
 from typing import List
-import random
 
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 from fastapi.responses import HTMLResponse
@@ -20,16 +19,12 @@ class ConnectionManager:
         self.client_ids: list[int] = []
         self.markers: dict = {}
 
-
     async def connect(self, websocket: WebSocket):
         await websocket.accept()
         self.active_connections.append(websocket)
 
     def disconnect(self, websocket: WebSocket):
         self.active_connections.remove(websocket)
-
-    async def send_personal_message(self, message: str, websocket: WebSocket):
-        await websocket.send_text(message)
 
     async def broadcast(self, client_id: int, coordinate: str):
         self.client_ids.append(client_id)
@@ -52,13 +47,7 @@ manager = ConnectionManager()
 async def get():
     with open("viewer.html", "r", encoding='utf-8') as f:
         html = f.read()
-
     return HTMLResponse(html)
-
-
-@app.post("/coordinate/")
-async def create_location(coordinate: Coordinate):
-    return coordinate
 
 
 @app.websocket("/ws/{client_id}")
@@ -67,12 +56,8 @@ async def websocket_endpoint(websocket: WebSocket, client_id: int):
     try:
         while True:
             text = await websocket.receive_text()
-            # await manager.send_personal_message(data, websocket)
-            # await manager.broadcast_msg(f"user {client_id}'s location: {data}")
-            # await manager.broadcast_loc(text)
-
             await manager.broadcast(client_id, text)
 
     except WebSocketDisconnect:
         manager.disconnect(websocket)
-        await manager.broadcast(f"Client #{client_id} left the chat")
+        del manager.markers[client_id]
